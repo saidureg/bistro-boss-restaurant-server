efee1e25cf90521782c774614262496594871267
saidureg <saidureg@gmail.com>
2023-11-18 18:57:51 +0600

    find cart based on the user email

--- index.js
+++ index.js
@@ -40,6 +40,14 @@ async function run() {
       res.send(result);
     });
 
+    // cart collection
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/carts", async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
